c236856ad97dab91022f4198dff04496ed77f1f8
Alicia Key <alicia.key@galvanize.com>
2017-11-06 14:08:42 -0700

    Our little app

--- src/App.js
+++ src/App.js
@@ -2,17 +2,39 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const fakeItems = [
+  {
+    id: 2,
+    name: 'Cobra computer',
+    description: 'In communist Romania, electrons were illegal.',
+    barcode: 'F742MCBEKCG',
+    img_url: 'http://www.old-computers.com/museum/photos/Felix_HC85_System_1.jpg'
+  },
+  {
+    id: 1,
+    name: 'Fender Blondie',
+    description: 'Really loud',
+    barcode: 'XRA0K3QXBH',
+    img_url: 'http://www.astrings.co.uk/images/product/a/amps-electric-valve-fender-vintage-reissue-%E2%80%9965-deluxe-blondi-320px-320px.png'
+  }
+]
+
 class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      items: fakeItems
+    }
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>Brutalist again</p>
       </div>
     );
   }
