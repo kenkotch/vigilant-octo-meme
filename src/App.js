320a2b33177a58242983ab815d7c426f7ee8b8f4
Alicia Key <alicia.key@galvanize.com>
2017-11-06 14:51:19 -0700

    Now it makes the stupid list

--- src/App.js
+++ src/App.js
@@ -28,6 +28,19 @@ class App extends Component {
     }
   }
 
+  async componentDidMount() {
+    const res = await fetch('http://localhost:8181/hoard')
+    const json = await res.json()
+    this.setState({
+      ...this.state,
+      items: json
+    })
+    this.setState({
+      ...this.state,
+      items: json
+    })
+  }
+
   render() {
     return (
       <div className="App">
@@ -39,3 +52,103 @@ class App extends Component {
 }
 
 export default App;
+
+/***
+import React, { Component } from 'react'
+import './App.css'
+import HoardingList from './HoardingList'
+import ItemDetails from './ItemDetails'
+import HoardOrModifyItem from './HoardOrModifyItem'
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      currentItem: -1,
+      items: []
+    }
+
+    this.blankItem = {
+      name: '',
+      description: '',
+      imgUrl: ''
+    }
+  }
+
+  barcodeString = () => Math.random().toString(36).substr(2).toUpperCase()
+
+  async componentDidMount() {
+    console.log(`>>> GET ${process.env.REACT_APP_API_URL}/hoarding`)
+    const res = await fetch(`${process.env.REACT_APP_API_URL}/hoarding`)
+    const json = await res.json()
+    this.setState({
+      ...this.state,
+      items: json
+    })
+  }
+
+  deleteItem = async (e, id) => {
+    e.preventDefault()
+    const dbId = this.state.items[id].id
+    await fetch(`${process.env.REACT_APP_API_URL}/hoarding/${dbId}`, {method: 'DELETE'})
+    const getRes = await fetch(`${process.env.REACT_APP_API_URL}/hoarding`)
+    const items = await getRes.json()
+    this.setState({
+      ...this.state,
+      items: items,
+      currentItem: -1
+    })
+  }
+
+  addItem = async (partialItem) => {
+    const item = {
+      ...partialItem,
+      barcode: this.barcodeString()
+    }
+    const url = `${process.env.REACT_APP_API_URL}/hoarding`
+    const opts = {
+      method: 'POST',
+      body: JSON.stringify(item),
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      }
+    }
+    await fetch(url, opts)
+    const getRes = await fetch(`${process.env.REACT_APP_API_URL}/hoarding`)
+    const items = await getRes.json()
+    this.setState({
+      ...this.state,
+      items: items,
+      currentItem: -1
+    })
+  }
+
+  showDetails = (e, id) => {
+    e.preventDefault()
+    this.setState({
+      items: [...this.state.items],
+      currentItem: id
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Tech Hoarder</h1>
+        </header>
+        <HoardingList showDetails={ this.showDetails } deleteItem={ this.deleteItem } items = { this.state.items }/>
+        <div className='below-list'>
+          <ItemDetails item={ this.state.currentItem === -1 ? { id: -1 } : this.state.items[this.state.currentItem] }/>
+          <h2>Go ahead, hoard something...</h2>
+          <HoardOrModifyItem item={ this.blankItem } addItem={ this.addItem }/>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default App
+***/
