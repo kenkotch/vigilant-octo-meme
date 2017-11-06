082cca4def6c8d9ab229b7905eb7a10d2fa6a031
Alicia Key <alicia.key@galvanize.com>
2017-11-06 14:13:15 -0700

    Getting the first components

--- src/App.js
+++ src/App.js
@@ -31,9 +31,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
+        <h1 className="App-title">Welcome to React</h1>
         <p>Brutalist again</p>
       </div>
     );
